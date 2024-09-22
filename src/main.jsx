6523f054d029dca87b9daedb7939daf1ca1e6ec9
Anupkjha2601 <rachitjha999@gmail.com>
2024-09-22 23:13:39 +0530

    Anup Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-xhi1aux2bmrltmi0.us.auth0.com"
+      clientId="P1v6JSaEOUXKRAX0uFwZgfjOCTPit5e3"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
