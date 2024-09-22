a94058cfaa9c691f6ec27cd6596891fa7dd5220d
Anupkjha2601 <rachitjha999@gmail.com>
2024-09-22 20:15:53 +0530

    Anup Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
-// import { useState } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { ToastContainer } from "react-toastify";
 
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import OurWork from "./pages/OurWork";
-import SolutionsPage from "./components/solutions/SolutionsPage";
-import BubblePage from "./components/solutions/page/Bubblepage";
+import Home from './pages/Home';
+import About from './pages/About';
+import OurWork from './pages/OurWork';
+import SolutionsPage from './components/solutions/SolutionsPage';
+import BubblePage from './components/solutions/page/Bubblepage';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import ForgotPassword from './components/ForgotPassword';
+import PrivateRoutes from './utils/PrivateRoutes';
+import CompleteRegister from './components/CompleteRegister';
+
 function App() {
-  return (
-    <>
-      <Router>
-        {/* Define Routes here */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/our-work" element={<OurWork />} />
-          <Route path="/solutions" element={<SolutionsPage />} />
-          <Route path="/description/:name" element={<BubblePage />} />
-        </Routes>
-      </Router>
-      {/* <Navbar/>
-      <GoToTopButton/>
-      <HeroSection/>
-      <CTA/>
-      <Solutions/>
-      <Testimonials/>
+  const { user } = useAuth0();
+  console.log('Current User', user);
 
-      <Companies/>
-      <Footer/> */}
-    </>
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* Redirect to home page on load */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/completeregister" element={<CompleteRegister />} />
+
+          {/* Protected Routes */}
+          <Route element={<PrivateRoutes />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/our-work" element={<OurWork />} />
+            <Route path="/solutions" element={<SolutionsPage />} />
+            <Route path="/description/:name" element={<BubblePage />} />
+          </Route>
+        </Routes>
+
+        <ToastContainer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
